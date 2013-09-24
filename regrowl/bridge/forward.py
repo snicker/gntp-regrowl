5310c27f61af1ccd153c9365103c396f50814a27
snicker <ngordon779@gmail.com>
2013-09-23 20:10:47 -0700

    Support for multiple machines as defined in the .regrowl config

--- regrowl/bridge/forward.py
+++ regrowl/bridge/forward.py
@@ -1,8 +1,13 @@
 """
 Forwards a notification or registration to another machine
 
-This is just a simple regrowler to show the basic structure and provide
-a simple debug output
+Pulls destinations from the .regrowl config in the
+[regrowl.bridge.forward.destinations] section. Each machine
+should have its own line with the following format:
+machinename = <hostname>,<port>,<password>
+
+Currently forwards all packets that come in to the destinations
+in the configuration.
 """
 
 from __future__ import absolute_import
@@ -10,8 +15,10 @@ from __future__ import absolute_import
 import logging
 import gntp.notifier
 import gntp.core
+import ConfigParser
 
 from regrowl.regrowler import ReGrowler
+from regrowl.cli import CONFIG_PATH
 
 logger = logging.getLogger(__name__)
 
@@ -21,33 +28,36 @@ __all__ = ['GrowlForwarder']
 class GrowlForwarder(ReGrowler):
     key = __name__
     valid = ['REGISTER', 'NOTIFY', 'SUBSCRIBE']
-    destaddr = "computername"
-    destport = 23053
-    destpass = "password"
-
-    def getnotifier(self):
-        return gntp.notifier.GrowlNotifier(hostname = self.destaddr, password = self.destpass) 
 
     def forwardpacket(self, packet):
-        notifier = self.getnotifier()
-        if self.destpass:
-            packet.set_password(self.destpass,'MD5')
-        notifier._send(packet.info['messagetype'],packet)
+        destinations = self.load_destinations()
+        for destination in destinations:
+            logger.info("Forwarding packet to " + destination[0] + ":" + destination[1] + "...")
+            notifier = gntp.notifier.GrowlNotifier(hostname = destination[0], port = int(destination[1]), password = destination[2])
+            if destination[2]:
+                packet.set_password(destination[2],'MD5')
+            notifier._send(packet.info['messagetype'],packet)
+
+    def load_destinations(self):
+        parser = ConfigParser.ConfigParser()
+        parser.read('/'.join(CONFIG_PATH)) #[TODO] this is not portable
+        conf_destinations = parser.items("regrowl.bridge.forward.destinations")
+        destinations = []
+        for destination, options in conf_destinations:
+            destinations.append(options.split(','))
+        return destinations
 
     def instance(self, packet):
         return None
 
     def register(self, packet):
         logger.info('Register')
-        print 'Registration Packet Forwarding:'
         self.forwardpacket(packet)
 
     def notify(self, packet):
         logger.info('Notify')
-        print 'Notification Packet Forwarding:'
         self.forwardpacket(packet)
 
     def subscribe(self, packet):
         logger.info('Subscribe')
-        print 'Subscribe Packet Forwarding:'
         self.forwardpacket(packet)
